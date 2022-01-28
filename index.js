c8993e8b4e1aca33a4bfae910a9d346ed124409b
rohitpim1 <pimrohit33@gmail.com>
2022-01-28 15:14:27 +0530

    calc

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(){
+    let x=2;
+    let y=3;
+    let ans;
+    ans=x+y;
+    document.getElementById("answer").innerHTML=ans;
+    console.log(ans);
+}
+
+function sub(){
+    let x=5;
+    let y=1;
+    let ans;
+    ans=x-y;
+    document.getElementById("answer").innerHTML=ans;
+
+    console.log(ans);
+
+}
+
+function mult(){
+    let x=2;
+    let y=3;
+    let ans;
+    ans=x*y;
+    document.getElementById("answer").innerHTML=ans;
+
+    console.log(ans);
+
+}
+
+function divi(){
+    let x=2;
+    let y=3;
+    let ans;
+    ans=x/y;
+    document.getElementById("answer").innerHTML=ans;
+
+    console.log(ans);
+
+}
